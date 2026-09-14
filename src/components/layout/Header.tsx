0d4d2d0ad83d7b734a522bb9144c065ef37e57ff
Raf0627 <rafaelfrossard076@gmail.com>
2026-09-14 16:56:54 -0300

    feat: adicionando logos

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -11,9 +11,9 @@ const navLinks = [
 export default function Header() {
   return (
     <header className="w-full fixed top-0 left-0 z-50" >
-      <div className="bg-cover bg-center py-6 px-8 lg:px-16 flex items-center justify-between bg-[#002A52]">
+      <div className="bg-cover bg-center py-6 px-8 lg:px-16 flex items-center justify-between bg-[var(--main-color)]">
         <a href="/" className="text-white font-bold text-2xl tracking-wide">
-          LOGO
+          <img src="/logo-horizontal-negativo.svg" className="w-40" alt="" />
         </a>
 
         <nav className="hidden md:flex items-center gap-10">
